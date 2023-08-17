49ac3464c06e446b5ed75edb752f3ef1a0c1caac
Tim Smith <tim.smith2814@gmail.com>
2023-08-17 15:22:59 +0100

    add page view count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <main>0</main>;
+import kv from "@vercel/kv";
+
+export default async function Home() {
+  // see how many people love us on GitHub
+  const res = await fetch("https://api.github.com/repos/Wollivan/view-counter", { next: { revalidate: 15 } });
+  const data = await res.json();
+
+  // see how many times this page has been viewed
+  const pageViews = await kv.incr("views");
+
+  // make the numbr get bigger for each view
+  const myStyles = {
+    p: {
+      fontSize: pageViews + "rem",
+    },
+  };
+
+  return (
+    <main>
+      <p style={myStyles.p}>{pageViews}</p>
+      <span>⭐ {data.stargazers_count}</span>
+    </main>
+  );
 }
